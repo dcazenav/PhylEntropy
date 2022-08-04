052869534d70338b5edfb4977f12fe42e1927feb
dcazenav <damien.cazenave.navis@gmail.com>
2022-08-04 09:16:31 -0400

    Update urls.py

--- phylogene_app/urls.py
+++ phylogene_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path('import_data/',views.import_data, name='phylogene_import'),
     path('ajax1/', views.ajax_1, name='phylogene_ajax1'),
     path('run_algo/', views.run_algo, name='phylogene_run_algo'),
-    path('test/', views.test, name='phylogene_test'),
+    #path('test/', views.test, name='phylogene_test'),
     path('base/', views.base, name='base'),
+    #path('chart/', views.chart, name='chart'),
 ]
