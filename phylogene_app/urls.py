f6abc3a8529b3dc675de4e28d705b74796f305af
linuxipg <damien.cazenave.navis@gmail.com>
2022-10-20 15:31:03 -0400

    static and allowed_hosts

--- phylogene_app/urls.py
+++ phylogene_app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns=[
-    path('', views.intro, name='intro'),
-    path('import_data/',views.import_data, name='phylogene_import'),
-    path('ajax1/', views.ajax_1, name='phylogene_ajax1'),
-    path('run_algo/', views.run_algo, name='phylogene_run_algo'),
-    #path('test/', views.test, name='phylogene_test'),
-    path('base/', views.base, name='base'),
-    #path('Rtest/', views.run_algo, name='Rtest'),
-    #path('chart/', views.chart, name='chart'),
-]
+urlpatterns = [
+                  path('', views.intro, name='intro'),
+                  path('import_data/', views.import_data, name='phylogene_import'),
+                  path('ajax1/', views.ajax_1, name='phylogene_ajax1'),
+                  path('run_algo/', views.run_algo, name='phylogene_run_algo'),
+                  # path('test/', views.test, name='phylogene_test'),
+                  path('base/', views.base, name='base'),
+                  # path('Rtest/', views.run_algo, name='Rtest'),
+                  # path('chart/', views.chart, name='chart'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
