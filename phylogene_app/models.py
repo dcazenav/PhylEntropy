41e7488bb039dbfb4789b64aa1d0fb7f71da3a17
linuxipg <damien.cazenave.navis@gmail.com>
2023-03-28 14:02:18 -0400

    création méthode delete

--- phylogene_app/models.py
+++ phylogene_app/models.py
@@ -5,9 +5,14 @@ from django.core.validators import FileExtensionValidator
 class UserFilesForm(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField("Enter username", max_length=50)
-#    file = models.FileField(upload_to='upload/', validators=[FileExtensionValidator( ['csv'] ) ])  # for creating file input
-    file = models.FileField(upload_to='upload/')  # for creating file input
+    # file = models.FileField(upload_to='upload/', validators=[FileExtensionValidator( ['csv'] ) ])  # for creating file input
+    file = models.FileField("Load .csv file", upload_to='upload/') # for creating file input
 
+
+    def delete_file(self):
+        print('model',self.file)
+        self.file.delete()
+        super().delete()
     class Meta:
         db_table = "userfile"
 
